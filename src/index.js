c48d94cb5a642c23b64ac156639d8a446299623f
Doroteo <navarretedoro@gmail.com>
2022-05-22 02:00:37 -0300

    testimonios

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./index.css";
+import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
